11ecee6653451d3cf54eb53ee1990f5f0df7d5b9
nik1168 <ngeisser32@gmail.com>
2019-04-27 19:40:46 +0200

    Implementing Feature Importance

--- FeatureImportance.py
+++ FeatureImportance.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
+from sklearn.ensemble import ExtraTreesClassifier
+import matplotlib.pyplot as plt
 
 """
 Feature importance gives you a score for each feature of your data, 
@@ -21,12 +21,10 @@ class FeatureImportance:
         X = self.data_set.iloc[:, 0:20]  # independent columns
         y = self.data_set.iloc[:, -1]  # target column i.e price range
 
-        # apply SelectKBest class to extract top 10 best features
-        bestfeatures = SelectKBest(score_func=chi2, k=10)
-        fit = bestfeatures.fit(X, y)
-        dfscores = pd.DataFrame(fit.scores_)
-        dfcolumns = pd.DataFrame(X.columns)
-        # concat two dataframes for better visualization
-        featureScores = pd.concat([dfcolumns, dfscores], axis=1)
-        featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
-        print(featureScores.nlargest(10, 'Score'))  # print 10 best features
+        model = ExtraTreesClassifier()
+        model.fit(X, y)
+        print(model.feature_importances_)  # use inbuilt class feature_importances of tree based classifiers
+        # plot graph of feature importances for better visualization
+        feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+        feat_importances.nlargest(10).plot(kind='barh')
+        plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from FeatureImportance import FeatureImportance
 from FileManager import FileManager
 from UnivariateSelection import UnivariateSelection
 
 if __name__ == "__main__":
     print("Here is where the magic of feature selection begins :)")
     file_manager = FileManager()
-    data_set_ = file_manager.get_data_train()
-    univariate_selection = UnivariateSelection(data_set_)
+    data_set = file_manager.get_data_train()
+    print(data_set.columns)
+    univariate_selection = UnivariateSelection(data_set)
     univariate_selection.exec()
+    feature_importance = FeatureImportance(data_set)
+    feature_importance.exec()
 
 
